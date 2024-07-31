b7749983fd91e002d711f6da09250dd6a43069bb
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-07-31 15:14:41 +0200

    added PersonalHomepage

--- src/core/App.js
+++ src/core/App.js
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { themeLight } from "./theme";
 import { Normalize } from "styled-normalize";
+import { PersonalHomepage } from "../features/PersonalHomepage";
 
 function App() {
 	return (
 		<ThemeProvider theme={themeLight}>
 			<Normalize />
 			<GlobalStyle />
+			<PersonalHomepage />
 		</ThemeProvider>
 	);
 }
