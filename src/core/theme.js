207e7278eac01a4eec706adce97f7f78c4f9ca4c
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-07-31 17:28:57 +0200

    changed color name from mineShaft to black

--- src/core/theme.js
+++ src/core/theme.js
@@ -2,14 +2,14 @@ const colorNames = {
 	scienceBlue: "#0366D6",
 	white: "#FFFFFF",
 	whiteLilac: "#FBFBFE",
-	mineShaft: "#252525",
+	black: "#252525",
 	slateGray: "#6E7E91",
 };
 
 export const themeLight = {
 	colors: {
 		primary: colorNames.scienceBlue,
-		textPrimary: colorNames.mineShaft,
+		textPrimary: colorNames.black,
 		site: {
 			background: colorNames.whiteLilac,
 			text: colorNames.slateGray,
