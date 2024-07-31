4185e51555758cc043d0aced571b3a486e363b43
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-07-31 14:40:38 +0200

    added styled-normalize

--- src/core/App.js
+++ src/core/App.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { themeLight } from "./theme";
+import { Normalize } from "styled-normalize";
 
 function App() {
 	return (
 		<ThemeProvider theme={themeLight}>
+			<Normalize />
 			<GlobalStyle />
 		</ThemeProvider>
 	);
