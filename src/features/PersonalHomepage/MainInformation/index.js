fc744523b9c31c4859ada87591945ee60249d186
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-07-31 17:31:03 +0200

    MainInformation added

---
+++ src/features/PersonalHomepage/MainInformation/index.js
@@ -0,0 +1,14 @@
+import annaLukaszczykProfile from "./profile.jpg";
+import { Wrapper, Photo, ThisIs, Name, Summary } from "./styled";
+
+export const MainInformation = () => (
+	<Wrapper>
+		<Photo src={annaLukaszczykProfile} alt="Zdjęcie Anny Łukaszczyk" />
+		<div>
+			<ThisIs>This is</ThisIs>
+			<Name>Anna Łukaszczyk</Name>
+            <Summary>👩‍🦰💻 I'm a passionate Frontend Developer with a strong interest in React, currently
+            looking for new job opportunities.</Summary>
+		</div>
+	</Wrapper>
+);
