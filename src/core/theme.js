04caf79e86626e71899e2377f3d51652744c1d8e
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-07-31 14:38:12 +0200

    added colorNames and themeLight

---
+++ src/core/theme.js
@@ -0,0 +1,18 @@
+const colorNames = {
+	scienceBlue: "#0366D6",
+	white: "#FFFFFF",
+	whiteLilac: "#FBFBFE",
+	mineShaft: "#252525",
+	slateGray: "#6E7E91",
+};
+
+export const themeLight = {
+	colors: {
+		primary: colorNames.scienceBlue,
+		textPrimary: colorNames.mineShaft,
+		site: {
+			background: colorNames.whiteLilac,
+			text: colorNames.slateGray,
+		},
+	},
+};
