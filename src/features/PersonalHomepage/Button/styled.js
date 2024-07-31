38c75d4694714b8483abd7b28a8fe0521588ea9d
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-07-31 18:15:42 +0200

    added styles for ButtonLink

---
+++ src/features/PersonalHomepage/Button/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ButtonLink = styled.a`
+	padding: 12px 16px;
+	background: ${({ theme }) => theme.colors.primary};
+    border-radius: 4px;
+`;
