8118dcb315f0f364081264e83cadbb438527a62c
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-07-31 14:37:30 +0200

    added GlobalStyle

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+	html {
+		box-sizing: border-box;
+	}
+
+	*, ::after, ::before {
+		box-sizing: inherit;
+	}
+
+	body {
+		font-family: "Inter", sans-serif;
+		background: ${({ theme }) => theme.colors.site.background};
+		color: ${({ theme }) => theme.colors.site.text};
+		font-size: 18px;
+		word-break: break-word;
+		padding-bottom: 108px;
+	}
+`;
