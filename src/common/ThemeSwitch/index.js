ee4a06f4f374cc764d59dcbaa6fc2b2ca6801ed8
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-01 17:40:37 +0200

    ThemeSwitch added

---
+++ src/common/ThemeSwitch/index.js
@@ -0,0 +1,14 @@
+import { Wrapper, Box, Text, Icon } from "./styled";
+
+export const ThemeSwitch = () => {
+	return (
+		<Wrapper>
+			<Box>
+				<Text>
+					Dark mode off
+				</Text>
+                <Icon />
+			</Box>
+		</Wrapper>
+	);
+};
