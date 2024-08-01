733a5bf8a7edc935991e1ed35ba74eea32331538
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-01 16:17:25 +0200

    changed "width" dor Wrapper to "Max-width" and added styles for Envelope

--- src/features/PersonalHomepage/MainInformation/styled.js
+++ src/features/PersonalHomepage/MainInformation/styled.js
@@ -1,17 +1,18 @@
 import styled from "styled-components";
+import { ReactComponent as envelope } from "../Button/envelope.svg";
 
 export const Wrapper = styled.header`
 	display: grid;
 	grid-template-columns: auto 1fr;
 	grid-gap: 72px;
 	align-items: center;
-    width: 1120px;
+	max-width: 1120px;
 `;
 
 export const Photo = styled.img`
 	width: 25vw;
-    max-width: 420px;
-    max-height: 420px;
+	max-width: 420px;
+	max-height: 420px;
 	border-radius: 50%;
 `;
 
@@ -22,14 +23,18 @@ export const ThisIs = styled.p`
 `;
 
 export const Name = styled.h1`
-margin-top: 12px;
+	margin-top: 12px;
 	font-size: 38px;
 	font-weight: 900;
 	color: ${({ theme }) => theme.colors.textPrimary};
 `;
 
 export const Summary = styled.p`
-margin: 36px 0 32px 0;
-font-size: 20px;
-line-height: 1.5;
+	margin: 36px 0 32px 0;
+	font-size: 20px;
+	line-height: 1.5;
+`;
+
+export const Envelope = styled(envelope)`
+	margin-right: 12px;
 `;
