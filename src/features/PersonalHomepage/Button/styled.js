7cbadb0eb05ada1c101f2a281c2a9b6f3562d9d1
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-01 16:15:22 +0200

    added styles for ButtonLink

--- src/features/PersonalHomepage/Button/styled.js
+++ src/features/PersonalHomepage/Button/styled.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const ButtonLink = styled.a`
+	display: inline-flex;
+	align-items: center;
 	padding: 12px 16px;
 	background: ${({ theme }) => theme.colors.primary};
-    border-radius: 4px;
+	color: ${({ theme }) => theme.colors.buttonLink.text};
+	border-radius: 4px;
+	cursor: pointer;
+	transition: box-shadow 0.3s;
+
+	&:hover {
+		box-shadow: 4px 4px 0 ${({ theme }) => theme.colors.buttonLink.shadow},
+			-4px -4px 0 ${({ theme }) => theme.colors.buttonLink.shadow},
+			4px -4px 0 ${({ theme }) => theme.colors.buttonLink.shadow},
+			-4px 4px 0 ${({ theme }) => theme.colors.buttonLink.shadow};
+	}
 `;
