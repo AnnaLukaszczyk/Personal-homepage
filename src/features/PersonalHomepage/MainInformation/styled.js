93e3c24620c530d57b05e90703905acb63ef82e7
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-03 12:07:47 +0200

    added @media queries to styled components

--- src/features/PersonalHomepage/MainInformation/styled.js
+++ src/features/PersonalHomepage/MainInformation/styled.js
@@ -7,6 +7,15 @@ export const Wrapper = styled.header`
 	grid-gap: 72px;
 	align-items: center;
 	max-width: 1120px;
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+		grid-gap: 32px;
+	}
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		grid-template-columns: 1fr;
+		grid-gap: 16px;
+	}
 `;
 
 export const Photo = styled.img`
@@ -14,6 +23,10 @@ export const Photo = styled.img`
 	max-width: 420px;
 	max-height: 420px;
 	border-radius: 50%;
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		width: 160px;
+	}
 `;
 
 export const ThisIs = styled.p`
@@ -27,14 +40,35 @@ export const Name = styled.h1`
 	font-size: 38px;
 	font-weight: 900;
 	color: ${({ theme }) => theme.colors.textPrimary};
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+		font-size: 32px;
+	}
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		font-size: 24px;
+	}
 `;
 
 export const Summary = styled.p`
 	margin: 36px 0 32px 0;
 	font-size: 20px;
 	line-height: 1.5;
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+		font-size: 18px;
+	}
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		margin: 16px 0 24px 0;
+		font-size: 16px;
+	}
 `;
 
 export const Envelope = styled(envelope)`
 	margin-right: 12px;
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+		margin-right: 10px;
+	}
 `;
