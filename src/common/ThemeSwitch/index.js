b7066d9e36e0482ff405ddd85d8cf997f91767f1
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-03 15:04:01 +0200

    added useSelector, useDispatch and Button

--- src/common/ThemeSwitch/index.js
+++ src/common/ThemeSwitch/index.js
@@ -1,14 +1,18 @@
-import { Wrapper, Box, Text, Icon } from "./styled";
+import { Wrapper, Box, Text, Button, Icon } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
 import { selectIsDarkTheme, toggleTheme } from "../themeSlice";
 
 export const ThemeSwitch = () => {
+	const isDarkTheme = useSelector(selectIsDarkTheme);
+	const dispatch = useDispatch();
+
 	return (
 		<Wrapper>
 			<Box>
-				<Text>
-					Dark mode off
-				</Text>
-                <Icon />
+				<Text>Dark mode {isDarkTheme ? "on" : "off"}</Text>
+				<Button onClick={() => dispatch(toggleTheme())}>
+					<Icon />
+				</Button>
 			</Box>
 		</Wrapper>
 	);
