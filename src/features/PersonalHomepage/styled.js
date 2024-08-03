50287bd5e18cbebd92d637ea0a172f0e48145cf4
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-03 12:02:57 +0200

    added @media to Container

--- src/features/PersonalHomepage/styled.js
+++ src/features/PersonalHomepage/styled.js
@@ -4,4 +4,9 @@ export const Container = styled.div`
 	padding: 0 40px;
 	max-width: 1216px;
 	margin: 120px auto;
+
+	@media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+			margin: 20px 0;
+			padding: 0 18px;
+		}
 `;
