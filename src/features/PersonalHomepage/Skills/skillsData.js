a61b1d74a08932c79dd7082a08f3c788c92fe0a6
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-05 17:28:26 +0200

    added skills and nextSkills

---
+++ src/features/PersonalHomepage/Skills/skillsData.js
@@ -0,0 +1,29 @@
+export const skills = [
+	"Semantic & accessible HTML",
+	"CSS Flexbox",
+	"CSS Grid",
+	"CSS BEM convention",
+	"Responsive Web Design",
+	"Git",
+	"GitHub Pull Requests & Review",
+	"JavaScript",
+	"Immutability",
+	"React",
+	"React Hooks",
+	"NPM",
+	"Markdown",
+	"Figma",
+	"Error handling",
+	"Working with API (fetch, axios)",
+	"Redux (Toolkit)",
+	"Promises, Async/Await",
+	"Redux-Saga",
+	"React Router",
+	"Trello",
+	"Teamwork",
+];
+
+export const nextSkills = [
+	"TypeScript",
+	"Angular",
+];
