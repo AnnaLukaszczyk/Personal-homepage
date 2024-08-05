50b8076a959d8eec09fb6ec6a6cc4297d5c67aa0
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-05 14:11:22 +0200

    added main and Skills

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -1,10 +1,15 @@
 import { ThemeSwitch } from "../../common/ThemeSwitch";
 import { MainInformation } from "./MainInformation";
+import { Skills } from "./Skilks";
 import { Container } from "./styled";
 
 export const PersonalHomepage = () => (
 	<Container>
 		<ThemeSwitch />
 		<MainInformation />
+		<main>
+			<Skills title={"My skillset includes 🛠️"}/>
+			<Skills title={"What I want to learn next 🚀"}/>
+		</main>
 	</Container>
 );
