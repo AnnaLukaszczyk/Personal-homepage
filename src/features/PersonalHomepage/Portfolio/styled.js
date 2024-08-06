900ea3bc93ff211b5837b1a2c903159885af7fab
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-06 14:15:19 +0200

    edded styled-components for Section, Header, StyledGithubIcon, HeaderTitle and MyRecentProjects

---
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { ReactComponent as GithubIcon } from "./github.svg";
+
+export const Section = styled.section`
+	margin-top: 72px;
+`;
+
+export const Header = styled.header`
+	text-align: center;
+`;
+
+export const StyledGithubIcon = styled(GithubIcon)`
+	color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const HeaderTitle = styled.h2`
+	margin: 12px 0 8px;
+	font-size: 30px;
+	font-weight: 900;
+	color: ${({ theme }) => theme.colors.textPrimary};
+`;
+
+export const MyRecentProjects = styled.h3`
+	margin: 0 0 24px;
+	font-size: 20px;
+	font-weight: normal;
+	color: ${({ theme }) => theme.colors.textPrimary};
+`;
