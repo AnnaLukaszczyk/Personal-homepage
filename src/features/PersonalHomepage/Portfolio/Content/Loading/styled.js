1edab1b4970f4c9a1ee341ff03ef846cf22c0bd6
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-06 16:57:26 +0200

    added responsive styles for mobileMax breakpoints

--- src/features/PersonalHomepage/Portfolio/Content/Loading/styled.js
+++ src/features/PersonalHomepage/Portfolio/Content/Loading/styled.js
@@ -9,14 +9,29 @@ to {
 
 export const Wrapper = styled.div`
 	margin: 88px;
+    min-width: 284px;
 	text-align: center;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		font-size: 17px;
+		margin: 32px 0 0;
+	}
 `;
+
 export const LoadingInfo = styled.p`
 	color: ${({ theme }) => theme.colors.textPrimary};
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		font-size: 15px;
+	}
 `;
 
 export const StyledSpinner = styled(Spinner)`
 	margin-top: 48px;
 	animation: ${rotate} 1s linear infinite;
 	color: ${({ theme }) => theme.colors.primary};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		margin-top: 16px;
+		width: 80px;
+	}
 `;
