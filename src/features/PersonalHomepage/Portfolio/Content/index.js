3e1e233fc4df089f97f616d5629624fad1f24888
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-09 14:03:05 +0200

    added Repositories component to Content

--- src/features/PersonalHomepage/Portfolio/Content/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/index.js
@@ -1,9 +1,11 @@
 import { Error } from "./Error";
 import { Loading } from "./Loading";
+import { Repositories } from "./Repositories";
 
 export const Content = () => (
 	<>
 		<Loading />
 		<Error />
+		<Repositories />
 	</>
 );
