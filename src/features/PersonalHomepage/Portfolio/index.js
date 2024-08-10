294fe218c138b9cc171314297ec10b04860d071f
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-10 15:04:00 +0200

    integrated Redux state and effects into Portfolio component

--- src/features/PersonalHomepage/Portfolio/index.js
+++ src/features/PersonalHomepage/Portfolio/index.js
@@ -1,19 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Content } from "./Content";
-import {
-	Section,
-	Header,
-	StyledGithubIcon,
-	HeaderTitle,
-	MyRecentProjects,
-} from "./styled";
+import { Section, Header, StyledGithubIcon, HeaderTitle, MyRecentProjects } from "./styled";
+import { githubUsername } from "./githubUsername"
+import { fetchRepositories, selectRepositories, selectRepositoriesStatus } from "../../personalHomepageSlice";
+import { useEffect } from "react";
 
-export const Portfolio = () => (
-	<Section>
-		<Header>
-			<StyledGithubIcon />
-			<HeaderTitle>Portfolio</HeaderTitle>
-			<MyRecentProjects>My recent projects</MyRecentProjects>
-		</Header>
-		<Content />
-	</Section>
-);
+export const Portfolio = () => {
+const dispatch = useDispatch();
+const repositoriesStatus = useSelector(selectRepositoriesStatus)
+const repositories = useSelector(selectRepositories)
+
+useEffect(() => {
+	dispatch(fetchRepositories(githubUsername));
+}, [dispatch]);
+
+	return (
+		<Section>
+			<Header>
+				<StyledGithubIcon />
+				<HeaderTitle>Portfolio</HeaderTitle>
+				<MyRecentProjects>My recent projects</MyRecentProjects>
+			</Header>
+			<Content status={repositoriesStatus} repositories={repositories} />
+		</Section>
+	);
+};
