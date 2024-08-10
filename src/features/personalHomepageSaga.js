bccd895f94a3abbfd059e35a8609dde6d9b472dc
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-10 15:28:00 +0200

    added loading delay and error handling to fetchRepositories saga

--- src/features/personalHomepageSaga.js
+++ src/features/personalHomepageSaga.js
@@ -1,13 +1,16 @@
-import { takeLatest, call, put } from "redux-saga/effects";
-import { fetchRepositories, fetchRepositoriesSuccess } from "./personalHomepageSlice";
+import { takeLatest, call, put, delay } from "redux-saga/effects";
+import { fetchRepositories, fetchRepositoriesError, fetchRepositoriesSuccess } from "./personalHomepageSlice";
 import { getRepositories } from "./personalHomepageAPI";
+
+const loadingDelay = 2_000;
 
 function* fetchRepositoriesHandler({ payload: username }) {
 	try {
+		yield delay(loadingDelay); // just to demo the loading
 		const repositories = yield call(getRepositories, username);
 		yield put(fetchRepositoriesSuccess(repositories));
 	} catch (error) {
-		
+		yield put(fetchRepositoriesError());
 	}
 }
 
