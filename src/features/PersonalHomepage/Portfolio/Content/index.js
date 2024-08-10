1144f80a555f91c5eaaad6d4a0ad4b47c2969064
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-10 15:32:10 +0200

    updated Content component to handle additional states and error reporting

--- src/features/PersonalHomepage/Portfolio/Content/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/index.js
@@ -4,14 +4,18 @@ import { Repositories } from "./Repositories";
 
 export const Content = ({ status, repositories }) => {
 	switch (status) {
+		case "initial":
+			return null;
+
+		case "loading":
+			return <Loading />;
+
+		case "error":
+			return <Error />;
+
 		case "success":
 			return <Repositories repositories={repositories} />;
 		default:
-			return (
-				<>
-					<Loading />
-					<Error />
-				</>
-			);
+			throw new Error(`incorrect status: ${status}`);
 	}
 };
