9028628bc78afbc39a447997bde8001cad5b7872
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-11 18:02:57 +0200

    added filtering of repositories by description

--- src/features/PersonalHomepage/Portfolio/Content/Repositories/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/Repositories/index.js
@@ -9,36 +9,42 @@ import {
 	Tile,
 } from "./styled";
 
-export const Repositories = ({ repositories }) => (
-	<List>
-		{repositories.map(({ id, name, description, homepage, html_url }) => (
-			<Tile key={id}>
-				<Name>{name}</Name>
-				<Description>{description}</Description>
-				<Links>
-					{!!homepage && (
-						<LinkRow>
-							<dt>Demo:</dt>
-							<LinkValue>
-								<Link
-									href={`https://annalukaszczyk.github.io/my-todo-list-react/`}
-									target="_blank"
-									rel="noreferrer">
-									https://annalukaszczyk.github.io/my-todo-list-react/
-								</Link>
-							</LinkValue>
-						</LinkRow>
-					)}
-					<LinkRow>
-						<dt>Code:</dt>
-						<LinkValue>
-							<Link target="_blank" rel="noreferrer" href={html_url}>
-								{html_url}
-							</Link>
-						</LinkValue>
-					</LinkRow>
-				</Links>
-			</Tile>
-		))}
-	</List>
-);
+export const Repositories = ({ repositories }) => {
+	const filteredRepositories = repositories.filter((repo) => repo.description);
+
+	return (
+		<List>
+			{filteredRepositories.map(
+				({ id, name, description, homepage, html_url }) => (
+					<Tile key={id}>
+						<Name>{name}</Name>
+						<Description>{description}</Description>
+						<Links>
+							{!!homepage && (
+								<LinkRow>
+									<dt>Demo:</dt>
+									<LinkValue>
+										<Link
+											href={`https://annalukaszczyk.github.io/my-todo-list-react/`}
+											target="_blank"
+											rel="noreferrer">
+											https://annalukaszczyk.github.io/my-todo-list-react/
+										</Link>
+									</LinkValue>
+								</LinkRow>
+							)}
+							<LinkRow>
+								<dt>Code:</dt>
+								<LinkValue>
+									<Link target="_blank" rel="noreferrer" href={html_url}>
+										{html_url}
+									</Link>
+								</LinkValue>
+							</LinkRow>
+						</Links>
+					</Tile>
+				)
+			)}
+		</List>
+	);
+};
