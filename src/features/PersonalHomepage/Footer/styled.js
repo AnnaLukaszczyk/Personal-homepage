258b136335138f9c6473b6c3e9a5c9a300933d63
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-11 14:35:11 +0200

    added styles for Footer component

---
+++ src/features/PersonalHomepage/Footer/styled.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.footer`
+	margin-top: 120px;
+	max-width: 680px;
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		margin-top: 48px;
+	}
+`;
+
+export const LetsTalk = styled.h2`
+	margin: 0;
+	margin-bottom: 24px;
+	font-size: 12px;
+	text-transform: uppercase;
+`;
+
+export const EmailLink = styled.a`
+	font-size: 32px;
+	font-weight: 900;
+	text-decoration: none;
+	color: ${({ theme }) => theme.colors.textPrimary};
+	transition: color 0.3s;
+
+	&:hover {
+		color: ${({ theme }) => theme.colors.primary};
+	}
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		font-size: 18px;
+	}
+`;
+
+export const Paragraph = styled.p`
+	margin-top: 24px;
+    font-size: 18px;
+	line-height: 1.4;
+	color: ${({ theme }) => theme.colors.textPrimary};
+
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+		font-size: 14px;
+	}
+`;
