8a4c8a0b5657cd8ec21b488b2f9353743ef09a04
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-13 19:43:41 +0200

    fixed code indentations

--- src/features/PersonalHomepage/Footer/index.js
+++ src/features/PersonalHomepage/Footer/index.js
@@ -6,9 +6,11 @@ export const Footer = () => (
 	<Wrapper>
 		<LetsTalk>Let's talk!</LetsTalk>
 		<EmailLink href={`mailto:${email}`}>{email}</EmailLink>
-        <Paragraph>
-				I'm always open to new projects whenever I have time. If you have a website, dashboard or a mobile app in mind and need some help to make your ideas come to life, feel free to contact me ✉️🤞
-			</Paragraph>
-            <SocialIcons />
+		<Paragraph>
+			I'm always open to new projects whenever I have time. If you have a
+			website, dashboard or a mobile app in mind and need some help to make your
+			ideas come to life, feel free to contact me ✉️🤞
+		</Paragraph>
+		<SocialIcons />
 	</Wrapper>
 );
