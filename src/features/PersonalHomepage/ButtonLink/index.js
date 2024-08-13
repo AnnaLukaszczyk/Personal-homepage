17a974347c3bb7a15d872265418ffe3b0079d91f
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-08-13 19:42:17 +0200

    Refactor: simplify Button component syntax

--- src/features/PersonalHomepage/ButtonLink/index.js
+++ src/features/PersonalHomepage/ButtonLink/index.js
@@ -1,9 +1,7 @@
 import { ButtonLink } from "./styled";
 
-export const Button = ({ href, title, target, children }) => {
-	return (
-		<ButtonLink href={href} title={title} target={target}>
-			{children}
-		</ButtonLink>
-	);
-};
+export const Button = ({ href, title, target, children }) => (
+	<ButtonLink href={href} title={title} target={target}>
+		{children}
+	</ButtonLink>
+);
